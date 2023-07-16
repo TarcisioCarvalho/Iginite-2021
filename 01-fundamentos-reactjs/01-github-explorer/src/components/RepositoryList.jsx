3d82eacb4339218ac0ac25756be1e5ebdf8f3753
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2023-07-15 21:24:02 -0300

    Utilizando o useEffect

--- 01-fundamentos-reactjs/01-github-explorer/src/components/RepositoryList.jsx
+++ 01-fundamentos-reactjs/01-github-explorer/src/components/RepositoryList.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import { RepositoryItem } from './RepositoryItem'
 import "../styles/repositories.scss";
-
+// https://api.github.com/orgs/rocketseat/repos
 const repository = {
     name:"ufornm2",
     description:"reacts/form",
     link:"www.github.com"
 }
 export const RepositoryList = () => {
-  const [repositorie,setRepositories] = React.useState();
+  
+    async function fetchGithub(){
+      const response = await fetch("https://api.github.com/orgs/rocketseat/repos");
+      const data = await response.json();
+      setRepositories(data);
+      console.log(data);
+    }
+
+  const [repositories,setRepositories] = React.useState();
+
+  React.useEffect(()=>{
+    fetchGithub();
+  },[]);
   return (
     <section className="repository-list">
         <h1>Lista de Repositórios</h1>
