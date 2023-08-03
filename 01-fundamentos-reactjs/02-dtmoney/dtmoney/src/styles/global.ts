0b5ca88af837c9bc661c737225ae3c520d666bda
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2023-08-03 20:14:32 -0300

    GlobalStyle

---
+++ 01-fundamentos-reactjs/02-dtmoney/dtmoney/src/styles/global.ts
@@ -0,0 +1,17 @@
+import {createGlobalStyle} from "styled-components";
+
+export const globalStyle = createGlobalStyle`
+:root{
+    --background:#f8f2f5;
+}
+
+*{
+    padding:0;
+    margin:0;
+    box-sizing:border-box;
+}
+body{
+
+}
+
+`;
