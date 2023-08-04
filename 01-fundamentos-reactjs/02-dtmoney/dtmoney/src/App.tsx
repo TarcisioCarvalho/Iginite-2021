3a165cee923bac490de7309cfa4e792416521e6f
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2023-08-04 19:42:03 -0300

    GlobalStyle css

--- 01-fundamentos-reactjs/02-dtmoney/dtmoney/src/App.tsx
+++ 01-fundamentos-reactjs/02-dtmoney/dtmoney/src/App.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { GlobalStyle } from "./styles/global";
 const Title = styled.h1`
 color:#8257e6;
 `;
@@ -6,7 +7,9 @@ export function App() {
   return (
     <div className="App">
       <Title>Hello World!</Title>
+      <GlobalStyle/>
     </div>
+    
   );
 }
 
--- 01-fundamentos-reactjs/02-dtmoney/dtmoney/src/styles/global.ts
+++ 01-fundamentos-reactjs/02-dtmoney/dtmoney/src/styles/global.ts
@@ -1,8 +1,14 @@
 import {createGlobalStyle} from "styled-components";
 
-export const globalStyle = createGlobalStyle`
+export const GlobalStyle = createGlobalStyle`
 :root{
     --background:#f8f2f5;
+    --red:#E52E4D;
+    --blue:#5429CC;
+    --blue-light:#6933FF;
+    --text-title:#363F5F;
+    --text-body:#969CB3;
+    --shape:#FFFFFF;
 }
 
 *{
@@ -10,8 +16,25 @@ export const globalStyle = createGlobalStyle`
     margin:0;
     box-sizing:border-box;
 }
-body{
 
+html{
+    @media (max-width:1080px) {
+        font-size:93.75%;
+    }
+    @media (max-width:720px) {
+        font-size:87.5%;
+    }
+}
+body{
+    background:var(--background);
+    -webkit-font-smoothing:antialiased;
 }
 
+button{
+    cursor: pointer;
+}
+[disabled]{
+    opacity:0.6;
+    cursor:not-allowed;
+}
 `;
