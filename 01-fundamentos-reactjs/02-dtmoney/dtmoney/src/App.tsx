1dac204da9961b32a199f6e340579beec15e4c35
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2023-08-31 21:07:30 -0300

    Component Header

--- 01-fundamentos-reactjs/02-dtmoney/dtmoney/src/App.tsx
+++ 01-fundamentos-reactjs/02-dtmoney/dtmoney/src/App.tsx
@@ -4,10 +4,10 @@ import { Header } from "./components/Header";
 
 export function App() {
   return (
-    <div className="App">
+    <>
       <Header/>
       <GlobalStyle/>
-    </div>
+    </>
     
   );
 }
--- 01-fundamentos-reactjs/02-dtmoney/dtmoney/src/components/Header/index.tsx
+++ 01-fundamentos-reactjs/02-dtmoney/dtmoney/src/components/Header/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import logo from "../../assets/Logo.svg";
 
 export const Header = () => {
   return (
    <header>
-    Header
+    <img src={logo} alt="Dt Money" />
+    <button type="button">
+      Nova Transação
+    </button>
    </header>
   )
 }
