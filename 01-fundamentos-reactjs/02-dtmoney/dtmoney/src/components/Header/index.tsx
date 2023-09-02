d96d6749c84cd79b6b25d096c488e19bf83431fc
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2023-09-01 21:25:32 -0300

    Component header

--- 01-fundamentos-reactjs/02-dtmoney/dtmoney/src/components/Header/index.tsx
+++ 01-fundamentos-reactjs/02-dtmoney/dtmoney/src/components/Header/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import logo from "../../assets/Logo.svg";
+import { Container, Content } from './styles';
 
 export const Header = () => {
   return (
-   <header>
-    <img src={logo} alt="Dt Money" />
-    <button type="button">
-      Nova Transação
-    </button>
-   </header>
+   <Container>
+    <Content>
+      <img src={logo} alt="Dt Money" />
+      <button type="button">
+        Nova Transação
+      </button>
+    </Content>
+   </Container>
   )
 }
--- 01-fundamentos-reactjs/02-dtmoney/dtmoney/src/components/Header/styles.ts
+++ 01-fundamentos-reactjs/02-dtmoney/dtmoney/src/components/Header/styles.ts
@@ -3,3 +3,8 @@ import styled from "styled-components";
 export const Container = styled.header`
     background:var(--blue);
 `;
+
+export const Content = styled.div`
+    max-width:1120px;
+    margin:0 auto;
+`;
